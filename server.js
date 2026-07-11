7cf38cdf08ec850bd541dc16026b35aed0e2d914
Demir Cücü <demircucu35@gmail.com>
2026-07-11 22:10:57 +0300

    feat: add additional endpoints and improve server structure

--- server.js
+++ server.js
@@ -2,15 +2,26 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
 
 app.get('/', (req, res) => {
     res.json({message: "Hello, my first backend server is working!"});
-})
+});
 
 app.get('/status', (req, res) => {
     res.json({status: "active", uptime: process.uptime()});
-})
+});
+
+app.get('/hello/:name', (req, res) => {
+    res.json({message: `Hello, ${req.params.name}!`});
+});
+
+app.post('/data', (req, res) => {
+    const inCommingData = req.body;
+    res.json({status: "Data received", received: inCommingData});
+});
 
 app.listen(PORT, () => {
     console.log(`Server is working in http://localhost:${PORT} address.`)
-})
+});
